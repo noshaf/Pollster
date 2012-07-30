c8496e985127df23e1bf2c48da1c349bd7f1d3ef
Noah Shafi <noshaf@Noahs-MacBook-Pro.local>
2012-07-30 11:37:24 -0700

    Added functionality to create a poll with a name.

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,14 +1,19 @@
 class PollsController < ApplicationController
   def new
+    @poll = Poll.new
   end
 
   def create
+    @poll = Poll.new(params[:poll])
+    @poll.save
+    redirect_to poll_path(@poll)
   end
 
   def edit
   end
 
   def show
+    @poll = Poll.find(params[:id])
   end
 
   def update
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,11 @@
 class Poll < ActiveRecord::Base
   attr_accessible :link, :name
+  before_save :create_link
+  
+  private
+  
+  def create_link
+    raise request.host
+  end
+  
 end
